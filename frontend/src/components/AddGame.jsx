c381795a1c8deda450a54e6799d0e46ee8a35487
Lionel Manzetti <lionel.manzetti@gmail.com>
2022-07-20 19:59:01 +0200

    Advanced on the adding new project part

--- frontend/src/components/AddGame.jsx
+++ frontend/src/components/AddGame.jsx
@@ -5,31 +5,65 @@ import ExportContextUser from "../contexts/UserContext";
 // eslint-disable-next-line react/prop-types
 function AddGame({ setModal, displayedGames }) {
   const { user } = useContext(ExportContextUser.UserContext);
+  const [selected, setSelected] = useState([]);
   const [boardgames, setBoardgames] = useState([]);
 
   useEffect(() => {
     axios
       .get(`${import.meta.env.VITE_BACKEND_URL}/boardgames/`)
-      .then((result) => setBoardgames(result.data))
+      .then((result) =>
+        setBoardgames(
+          result.data.filter((game) =>
+            // eslint-disable-next-line react/prop-types
+            displayedGames.find((bg) => bg.boardgame_id === game.boardgame_id)
+          )
+        )
+      )
       .catch((err) => console.warn(err));
   }, [user]);
 
+  const handleSelected = (data) => {
+    setSelected([...selected, data]);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // console.log(selected);
+  };
+
   return (
-    <div>
-      <h2>Add a new boardgame to your collection :</h2>
+    <div className="m-4 flex flex-col">
+      <h2>Add one or many boardgames to your collection :</h2>
       {boardgames && (
-        <div className="border-2 border-gray-200 h-1/3 overflow-scroll">
-          {boardgames
-            .filter((game) =>
-              // eslint-disable-next-line react/prop-types
-              displayedGames.find((bg) => bg.id === game.id)
-            )
-            .map((e) => (
-              <p key={e.id}>{e.name}</p>
+        <form onSubmit={handleSubmit} className="flex flex-col">
+          <select multiple size={5}>
+            {boardgames.map((b) => (
+              <option
+                key={b.name}
+                onClick={() =>
+                  handleSelected({ id: b.boardgame_id, name: b.name })
+                }
+              >
+                {b.name}
+              </option>
             ))}
-        </div>
+          </select>
+          <button
+            type="submit"
+            className="px-2 py-1 bg-gray-400 m-2 rounded-lg text-white"
+          >
+            Save
+          </button>
+        </form>
       )}
-      <button type="button" onClick={() => setModal(false)}>
+      <div className="flex justify-around">
+        {selected &&
+          selected.map((selection) => (
+            <button type="button">{selection.name}</button>
+          ))}
+      </div>
+
+      <button type="button" onClick={() => setModal(false)} className="mt-4">
         X
       </button>
     </div>
